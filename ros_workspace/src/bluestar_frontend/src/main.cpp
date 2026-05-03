5b8065b575d282c53e823f392267b2d2717735fe
Peyton <66343323+Peyton-C@users.noreply.github.com>
2026-05-02 23:38:22 -0230

    Fix servo behavior pt 12

--- ros_workspace/src/bluestar_frontend/src/main.cpp
+++ ros_workspace/src/bluestar_frontend/src/main.cpp
@@ -43,10 +43,14 @@ bool flipCam2HorizontallyButtonPressedLatch = false;
 bool flipCam3HorizontallyButtonPressedLatch = false;
 bool flipCam4HorizontallyButtonPressedLatch = false;
 
-bool servo_1_latch = false;
-bool servo_2_latch = false;
-bool servo_3_latch = false;
-bool servo_4_latch = false;
+bool servo_1_cw_latch = false;
+bool servo_2_cw_latch = false;
+bool servo_3_cw_latch = false;
+bool servo_4_cw_latch = false;
+bool servo_1_ccw_latch = false;
+bool servo_2_ccw_latch = false;
+bool servo_3_ccw_latch = false;
+bool servo_4_ccw_latch = false;
 
 bool brighten_led_latch_1 = false;
 bool brighten_led_latch_2 = false;
@@ -135,6 +139,11 @@ int main(int argc, char **argv) {
     cam3.start();
     cam4.start();
 
+    int Servo1Angle = 127;
+    int Servo2Angle = 127;
+    int Servo3Angle = 127;
+    int Servo4Angle = 127;
+
     //render loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -172,11 +181,6 @@ int main(int argc, char **argv) {
         bool Servo_4_CCW_Pressed = false;
         bool fast_mode_toggle = false;
         bool invert_controls_toggle = false;
-
-        int Servo1Angle = 127;
-        int Servo2Angle = 127;
-        int Servo3Angle = 127;
-        int Servo4Angle = 127;
 
         //control loop
         if (glfwJoystickPresent(GLFW_JOYSTICK_1) || keyboard_mode)
@@ -448,24 +452,28 @@ int main(int argc, char **argv) {
                 flipCam4HorizontallyButtonPressedLatch = false;
             }
 
-            bool servo_latches[] = {servo_1_latch, servo_2_latch, servo_3_latch, servo_4_latch};
+            bool servo_latches_cw[] = {servo_1_cw_latch, servo_2_cw_latch, servo_3_cw_latch, servo_4_cw_latch};
+            bool servo_latches_ccw[] = {servo_1_ccw_latch, servo_2_ccw_latch, servo_3_ccw_latch, servo_4_ccw_latch};
             int servo_angle_val[] = {Servo1Angle, Servo2Angle, Servo3Angle, Servo4Angle};
             bool servo_cw_state[] = {Servo_1_CW_Pressed, Servo_2_CW_Pressed, Servo_3_CW_Pressed, Servo_4_CW_Pressed};
             bool servo_ccw_state[] = {Servo_1_CCW_Pressed, Servo_2_CCW_Pressed, Servo_3_CCW_Pressed, Servo_4_CCW_Pressed};
 
             for (int i = 0; i < 4; i++) {
                 if (servo_cw_state[i]) {
-                    if (!servo_latches[i]) {
+                    if (!servo_latches_cw[i]) {
                         servo_angle_val[i] = (servo_angle_val[i] + SERVO_FREQ_INCREMENT > 255) ? 255 : servo_angle_val[i] + SERVO_FREQ_INCREMENT;
-                        servo_latches[i] = true;
-                    }
-                } else if (servo_ccw_state[i]) {
-                    if (!servo_latches[i]) {
-                        servo_angle_val[i] = (servo_angle_val[i] < SERVO_FREQ_INCREMENT) ? 0 : servo_angle_val[i] - SERVO_FREQ_INCREMENT;
-                        servo_latches[i] = true;
+                        servo_latches_cw[i] = true;
                     }
                 } else {
-                    servo_latches[i] = false;
+                    servo_latches_cw[i] = false;
+                }
+                if (servo_ccw_state[i]) {
+                    if (!servo_latches_ccw[i]) {
+                        servo_angle_val[i] = (servo_angle_val[i] < SERVO_FREQ_INCREMENT) ? 0 : servo_angle_val[i] - SERVO_FREQ_INCREMENT;
+                        servo_latches_ccw[i] = true;
+                    }
+                } else {
+                    servo_latches_ccw[i] = false;
                 }
             }
             
